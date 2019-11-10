59393e5746ac559f0536e803f5162611ef8b00c8
psr1919plus21 <psr1919plus21@gmail.com>
2019-11-10 18:37:10 +0300

    :rocket: [client] Labels for form inputs.

--- src/login.js
+++ src/login.js
@@ -4,6 +4,7 @@ import Api from './services/Api';
 import initCountiesSelect  from './components/countriesSelect';
 
 initCountiesSelect('.country-input');
+const phoneFormItem = document.querySelector('.phone-form-item');
 const phoneInput = document.querySelector('.phone-input');
 
 phoneInput.addEventListener('keyup', debounce(onPhoneKeyUp, 1000));
@@ -13,6 +14,7 @@ function onPhoneKeyUp(e) {
 
     // Show spinner
     phoneInput.classList.add('progress');
+    phoneFormItem.classList.remove('error');
     Api.sentPhone(value)
         .then(onSentPhoneSuccess)
         .catch(onSentPhoneError);
@@ -20,12 +22,15 @@ function onPhoneKeyUp(e) {
 
 function onSentPhoneSuccess() {
     console.log('Success');
+    phoneInput.classList.remove('progress');
     // Hide spinner
     // Show next button
 }
 
 function onSentPhoneError() {
     console.log('do error stuff');
+    phoneInput.classList.remove('progress');
+    phoneFormItem.classList.add('error');
     // Hide spinner
     // Show form with phone again 
 }
